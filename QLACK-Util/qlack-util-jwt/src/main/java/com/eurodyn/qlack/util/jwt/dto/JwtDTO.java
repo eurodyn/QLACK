261c68334f5191c2762024bda81ff0aea493d8d4
Nassos Michas <nassosmichas@me.com>
2018-06-06 21:09:01 +0300

    Add support for a 2FA indicator in JWT.

--- QLACK-Util/qlack-util-jwt/src/main/java/com/eurodyn/qlack/util/jwt/dto/JwtDTO.java
+++ QLACK-Util/qlack-util-jwt/src/main/java/com/eurodyn/qlack/util/jwt/dto/JwtDTO.java
@@ -4,7 +4,11 @@ package com.eurodyn.qlack.util.jwt.dto;
  * A placeholder for a JWT.
  */
 public class JwtDTO {
+  // The JWT to include.
   private String jwt;
+
+  // A helper flag to indicate that 2FA is required.
+  private boolean requires2FA;
 
   public JwtDTO(String jwt) {
     this.jwt = jwt;
@@ -16,5 +20,13 @@ public class JwtDTO {
 
   public void setJwt(String jwt) {
     this.jwt = jwt;
+  }
+
+  public boolean isRequires2FA() {
+    return requires2FA;
+  }
+
+  public void setRequires2FA(boolean requires2FA) {
+    this.requires2FA = requires2FA;
   }
 }
