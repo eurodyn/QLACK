9fd869630bccb17924e7432da94f6f25721983df
Nassos Michas <nassosmichas@me.com>
2018-07-18 02:46:38 +0300

    Ignore primitives.

--- QLACK-Util/qlack-util-data/src/main/java/com/eurodyn/qlack/util/data/fields/HideFieldUtil.java
+++ QLACK-Util/qlack-util-data/src/main/java/com/eurodyn/qlack/util/data/fields/HideFieldUtil.java
@@ -1,18 +1,27 @@
 package com.eurodyn.qlack.util.data.fields;
 
+import org.springframework.beans.BeanUtils;
+
 import java.lang.reflect.Field;
 
 public class HideFieldUtil {
   /**
-   * Sets a named field to null.
+   * Sets a named field to null. This is a pretty basic implementation only taking care of simple
+   * cases. A better alternative based on SpEL (or similar) should be implemented in the future.
    *
    * @param object The object to process.
    * @param fieldName The name of the field to set to null.
    */
   public static void nullifyField(Object object, String fieldName)
       throws NoSuchFieldException, IllegalAccessException {
+
     // Get a reference to the field to nullify.
     final Field field = object.getClass().getDeclaredField(fieldName);
+
+    // Skip primitive types since they can not be set to null.
+    if (!BeanUtils.isSimpleProperty(field.getClass())) {
+      return;
+    }
 
     // Make the field accessible.
     final boolean isAccessible = field.isAccessible();
