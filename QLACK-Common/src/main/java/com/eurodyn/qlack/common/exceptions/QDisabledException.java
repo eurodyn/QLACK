6bac2f8d662de69777be9ba861dd459b47655741
Nassos Michas <nassosmichas@me.com>
2018-11-19 17:05:55 +0200

    Added new Exception.

---
+++ QLACK-Common/src/main/java/com/eurodyn/qlack/common/exceptions/QDisabledException.java
@@ -0,0 +1,23 @@
+package com.eurodyn.qlack.common.exceptions;
+
+/**
+ * A generic exception representing a "disabled" condition.
+ */
+public class QDisabledException extends QException {
+
+  public QDisabledException() {
+    super();
+  }
+
+  public QDisabledException(String message) {
+    super(message);
+  }
+
+  public QDisabledException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public QDisabledException(String message, Object... args) {
+    super(message, args);
+  }
+}
