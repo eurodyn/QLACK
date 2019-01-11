8a7f22b29677e18b4d4902b01d205a9445970c84
Roumpen Gkoltouchtsian <roumpen.gkoltouchtsian@eurodyn.com>
2019-01-11 12:07:30 +0200

    Fixes on boot error

--- QLACK-Fuse/qlack-fuse-demo/src/main/java/com/eurodyn/qlack/fuse/demo/war/QlackFuseDemoApplication.java
+++ QLACK-Fuse/qlack-fuse-demo/src/main/java/com/eurodyn/qlack/fuse/demo/war/QlackFuseDemoApplication.java
@@ -3,10 +3,12 @@ package com.eurodyn.qlack.fuse.demo.war;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableCaching
 @EnableJpaRepositories("com.eurodyn.qlack.fuse.aaa.repository")
 @EntityScan("com.eurodyn.qlack.fuse.aaa.model")
 @ComponentScan(basePackages = {
