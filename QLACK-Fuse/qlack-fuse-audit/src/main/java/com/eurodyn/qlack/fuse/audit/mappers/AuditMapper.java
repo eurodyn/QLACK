6d554efc6ee176be30bdc31ef65fc5cb12057a36
Dimitris Papadeas <Dimitris.Papadeas@eurodyn.com>
2019-02-06 12:22:09 +0200

    Fixes Audit mapping issue

--- QLACK-Fuse/qlack-fuse-audit/src/main/java/com/eurodyn/qlack/fuse/audit/mappers/AuditMapper.java
+++ QLACK-Fuse/qlack-fuse-audit/src/main/java/com/eurodyn/qlack/fuse/audit/mappers/AuditMapper.java
@@ -3,6 +3,7 @@ package com.eurodyn.qlack.fuse.audit.mappers;
 import com.eurodyn.qlack.fuse.audit.dto.AuditDTO;
 import com.eurodyn.qlack.fuse.audit.model.Audit;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 import org.springframework.data.domain.Page;
 
@@ -13,5 +14,9 @@ public interface AuditMapper extends AuditBaseMapper<Audit, AuditDTO> {
   default Page<AuditDTO> toAuditDTO(Page<Audit> audits) {
     return audits.map(this::mapToDTO);
   }
+  
+  @Override
+  @Mapping(target = "level", source = "levelId.name")
+  AuditDTO mapToDTO(Audit audit);
 
 }
