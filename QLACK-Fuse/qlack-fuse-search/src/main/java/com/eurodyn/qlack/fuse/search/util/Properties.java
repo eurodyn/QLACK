78bbe8b729f7346f594f08712ec119bbb53d272d
Roumpen Gkoltouchtsian <roumpen.gkoltouchtsian@eurodyn.com>
2019-03-21 16:55:42 +0200

    Minor code style change

--- QLACK-Fuse/qlack-fuse-search/src/main/java/com/eurodyn/qlack/fuse/search/util/Properties.java
+++ QLACK-Fuse/qlack-fuse-search/src/main/java/com/eurodyn/qlack/fuse/search/util/Properties.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
 @Setter
 @Configuration
 @ConfigurationProperties(prefix = "qlack.fuse.search")
-
 public class Properties {
 
   private String esHosts;
