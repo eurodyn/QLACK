9d97e0d329bf784bb0921025d11c4c25742fdd51
Nassos Michas <nassosmichas@me.com>
2019-04-12 09:44:00 +0300

    Get/Set multiple settings at once.

--- QLACK-Fuse/qlack-fuse-settings/src/main/java/com/eurodyn/qlack/fuse/settings/mappers/SettingMapper.java
+++ QLACK-Fuse/qlack-fuse-settings/src/main/java/com/eurodyn/qlack/fuse/settings/mappers/SettingMapper.java
@@ -3,22 +3,24 @@ package com.eurodyn.qlack.fuse.settings.mappers;
 import com.eurodyn.qlack.fuse.settings.dto.GroupDTO;
 import com.eurodyn.qlack.fuse.settings.dto.SettingDTO;
 import com.eurodyn.qlack.fuse.settings.model.Setting;
-import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface SettingMapper {
 
-    List<SettingDTO> map(List<Setting> o);
+  List<SettingDTO> map(List<Setting> o);
 
-    SettingDTO map(Setting o);
+  SettingDTO map(Setting o);
 
-    @Mapping(source = "group", target = "name")
-    GroupDTO mapToGroupDTO(Setting o);
+  @Mapping(source = "group", target = "name")
+  GroupDTO mapToGroupDTO(Setting o);
 
-    List<GroupDTO> mapToGroupDTO(List<Setting> o);
+  List<GroupDTO> mapToGroupDTO(List<Setting> o);
 
-    @Mapping(source = "id", target = "id", ignore = true)
-    Setting mapToEntity(SettingDTO dto);
+  @Mapping(source = "id", target = "id", ignore = true)
+  @Mapping(target = "dbversion", ignore = true)
+  Setting mapToEntity(SettingDTO dto);
 }
