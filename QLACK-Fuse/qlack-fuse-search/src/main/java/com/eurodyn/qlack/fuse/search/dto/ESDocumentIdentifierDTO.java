e8faa27c8db2c8ec61b9532c5c9ff72c5296f7d6
Ioannis Mousmoutis <ioannis.mousmoutis@eurodyn.com>
2019-04-30 12:13:45 +0300

    Updated javadocs

--- QLACK-Fuse/qlack-fuse-search/src/main/java/com/eurodyn/qlack/fuse/search/dto/ESDocumentIdentifierDTO.java
+++ QLACK-Fuse/qlack-fuse-search/src/main/java/com/eurodyn/qlack/fuse/search/dto/ESDocumentIdentifierDTO.java
@@ -34,7 +34,7 @@ public class ESDocumentIdentifierDTO implements Serializable {
   protected String id;
 
   /**
-   * If set to true then wait for the changes made by the request to be made visible by a refresh before replying. This doesn’t force an
+   * If set to true then wait for the changes made by the request to be made visible by a refresh before replying. This does not force an
    * immediate refresh, rather, it waits for a refresh to happen. Elasticsearch automatically refreshes shards that have changed every
    * index.refresh_interval which defaults to one second. That setting is dynamic. Calling the Refresh API or setting refresh to true on
    * any of the APIs that support it will also cause a refresh, in turn causing already running requests with refresh=wait_for to return.
