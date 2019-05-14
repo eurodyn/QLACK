96f8a210b5ad2978a6a89889535f38804d5a9a7f
Theo Kliaris <theodoros.kliaris@gmail.com>
2019-05-14 14:35:26 +0300

    Add default dummy EncryptorDecryptor implementation

---
+++ QLACK-Util/qlack-util-data/src/main/java/com/eurodyn/qlack/util/data/encryption/EncryptDecryptDefaultImplementation.java
@@ -0,0 +1,27 @@
+package com.eurodyn.qlack.util.data.encryption;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * A dummy implementation of the {@link EncryptorDecryptor} that does nothing special but simply return the provided
+ * input for both {@link #encrypt(String)} and {@link #decrypt(String)} methods.
+ */
+@Component
+public class EncryptDecryptDefaultImplementation implements EncryptorDecryptor {
+  
+  /**
+   * {@inheritDoc}
+   */
+	@Override
+	public String encrypt(String message) {
+		return message;
+	}
+  
+  /**
+   * {@inheritDoc}
+   */
+	@Override
+	public String decrypt(String ciphertext) {
+		return ciphertext;
+	}
+}
