a663eb0ada5100c68e4c3aeef88816fe9359ee07
Nassos Michas <nassosmichas@me.com>
2019-05-15 01:28:44 +0300

    Javadoc.

--- QLACK-Util/qlack-util-data/src/main/java/com/eurodyn/qlack/util/data/encryption/EncryptDecryptDefaultImplementation.java
+++ QLACK-Util/qlack-util-data/src/main/java/com/eurodyn/qlack/util/data/encryption/EncryptDecryptDefaultImplementation.java
@@ -3,25 +3,27 @@ package com.eurodyn.qlack.util.data.encryption;
 import org.springframework.stereotype.Component;
 
 /**
- * A dummy implementation of the {@link EncryptorDecryptor} that does nothing special but simply return the provided
- * input for both {@link #encrypt(String)} and {@link #decrypt(String)} methods.
+ * A dummy implementation of the {@link EncryptorDecryptor} that does nothing special but simply
+ * return the provided input for both {@link #encrypt(String)} and {@link #decrypt(String)} methods.
+ * Provide your own implementation on your own application marking it with {@link
+ * org.springframework.context.annotation.Primary}.
  */
 @Component
 public class EncryptDecryptDefaultImplementation implements EncryptorDecryptor {
-  
+
   /**
    * {@inheritDoc}
    */
-	@Override
-	public String encrypt(String message) {
-		return message;
-	}
-  
+  @Override
+  public String encrypt(String message) {
+    return message;
+  }
+
   /**
    * {@inheritDoc}
    */
-	@Override
-	public String decrypt(String ciphertext) {
-		return ciphertext;
-	}
+  @Override
+  public String decrypt(String ciphertext) {
+    return ciphertext;
+  }
 }
